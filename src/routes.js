47a8ae7f04a790555a40b8f01f98d4e93760f70b
fauez <fauez.passos@inpe.br>
2020-07-14 22:52:42 -0300

    add redirect route do / if not exists

--- src/routes.js
+++ src/routes.js
@@ -8,16 +8,6 @@ resolver.setServers(['150.163.55.128']);
 
 const router = Router();
 
-// const ignoreRoutes = (req, res, next) => {
-//   if (req.url !== '/getip/*' || req.url !== '/getname/*') {
-//     return res.redirect('/');
-//   }
-  
-//   next();
-// };
-
-// router.use(ignoreRoutes);
-
 router.get('/', (req, res) => {
   return res.send({ usage: 'Only /getip or /getname routes' });
 });
@@ -27,7 +17,7 @@ router.get('/getip', (req, res) => {
 
   if (!name) {
     return res.status(400)
-      .json({ error: "Mandatory send dns name, EX: 'getip/?name=www.inpe.br'" });
+      .json({ error: "Mandatory send dns name, EX: 'getip/?name=www.inpe.br' " });
   }
 
   resolver.resolve4(name, (err, addressIp) => {
@@ -47,7 +37,7 @@ router.get('/getname', (req, res) => {
 
   if (!ip) {
     return res.status(400)
-      .json({ Usage: "Mandatory send Ip address, EX: '/getname/?ip=150.163.105.29'" });
+      .json({ Usage: "Mandatory send Ip address, EX: '/getname/?ip=150.163.105.29' " });
   }
   
   if (!isIp(ip)) {
@@ -65,6 +55,10 @@ router.get('/getname', (req, res) => {
       name: addressName[0]
     });
   });
+});
+
+router.get('*', (req, res) => {
+  res.redirect('/');
 });
 
 module.exports = router;
